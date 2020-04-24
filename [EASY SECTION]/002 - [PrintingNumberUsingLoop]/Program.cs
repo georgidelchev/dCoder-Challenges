a3469d849585abe8e7bb1ed0b3609c7097ce1156
georgidelchev <alienguymc@gmail.com>
2020-04-24 22:41:51 +0300

    add files via upload

---
+++ [EASY SECTION]/002 - [PrintingNumberUsingLoop]/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PrintingNumbersUsingLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
