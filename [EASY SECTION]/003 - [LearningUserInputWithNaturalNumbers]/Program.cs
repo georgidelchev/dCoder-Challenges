9f8f8871bdb45d311665701ded79e2d8d67884c5
georgidelchev <alienguymc@gmail.com>
2020-04-24 22:54:35 +0300

    add files via upload

---
+++ [EASY SECTION]/003 - [LearningUserInputWithNaturalNumbers]/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LearningUserInputWithNaturalNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberInput = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= numberInput; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
