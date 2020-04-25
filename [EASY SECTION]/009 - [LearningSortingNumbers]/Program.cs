3e478beb2d4eb9696f201bc1c5555550d49d2fab
georgidelchev <alienguymc@gmail.com>
2020-04-25 16:50:19 +0300

    add files via upload

---
+++ [EASY SECTION]/009 - [LearningSortingNumbers]/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace LearningSortingNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] sortedNumbers = numbers.OrderBy(i => i).ToArray();
+            foreach (var item in sortedNumbers)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+    }
+}
