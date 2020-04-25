0239462f3eece4c7d26a270ba026dd4a21e43b2b
georgidelchev <alienguymc@gmail.com>
2020-04-25 12:12:24 +0300

    add files via upload

---
+++ [EASY SECTION]/004 - [LearningFactorialsWithRecursion]/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace LearningFactorialsWithLoopRecursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int fact = Enumerable.Range(1, number).Aggregate((acc, x) => acc * x);
+            Console.WriteLine(fact);
+        }
+    }
+}
