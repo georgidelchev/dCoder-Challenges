f9947cb363641985ba5abde46f4bb0208236900a
georgidelchev <alienguymc@gmail.com>
2020-04-25 17:56:09 +0300

    add files via upload

---
+++ [EASY SECTION]/012 - [LearnSumOfNumbers]/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace LearnSumOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
