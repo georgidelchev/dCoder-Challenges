434c4b9e764f2a109151a52a610ad1ef0f24bc94
georgidelchev <alienguymc@gmail.com>
2020-04-25 17:52:35 +0300

    add files via upload

---
+++ [EASY SECTION]/011 - [TheLightSwitch]/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace TheLightSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] combination = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int theLightBulbIsOn = 1;
+            int theLightBulbIsOff = 0;
+
+            if ((combination[0] == 1 && combination[1] == 0) ||
+                (combination[0] == 0 && combination[1] == 1))
+            {
+                Console.WriteLine(theLightBulbIsOn);
+            }
+            else
+            {
+                Console.WriteLine(theLightBulbIsOff);
+            }
+        }
+    }
+}
