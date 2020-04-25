b042c172da7acba4c84135d0730d3fcf2b871c1d
georgidelchev <alienguymc@gmail.com>
2020-04-25 18:04:10 +0300

    add files via upload

---
+++ [EASY SECTION]/013 - [SumOfSquares]/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace SumOfSquares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int lastValue = numbers[numbers.Length - 1];
+            int sum = 0;
+
+            for (int i = numbers[0]; i <= lastValue; i++)
+            {
+                sum += i * i;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
