ce20ca4866c55fe5b0dc672f45d6e996ceb4aa12
georgidelchev <alienguymc@gmail.com>
2020-04-25 13:07:35 +0300

    add files via upload

---
+++ [EASY SECTION]/007 - [TheBlackJackWithConditions]/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace TheBlackJackWithConditions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            if (numbers[0] == 11)
+            {
+                numbers[0] = 1;
+            }
+
+            if (numbers[1] == 11)
+            {
+                numbers[1] = 1;
+            }
+
+            int sum = 0;
+            foreach (var nums in numbers)
+            {
+                sum += nums;
+            }
+
+            if (sum > 20)
+            {
+                sum = 0;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
