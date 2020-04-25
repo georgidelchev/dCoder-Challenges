d3debbcd85a5cacdc1ab60d172986be974d64b17
georgidelchev <alienguymc@gmail.com>
2020-04-25 12:16:32 +0300

    add files via upload

---
+++ [EASY SECTION]/005 - [HowManyWeeks]/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HowManyWeeks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int weeklyWaterUsage = int.Parse(Console.ReadLine());
+            int gallonsTheyHave = 10000;
+            int totalWeeks = gallonsTheyHave / weeklyWaterUsage;
+            Console.WriteLine(totalWeeks);
+        }
+    }
+}
