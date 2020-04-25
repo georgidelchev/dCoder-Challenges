2a192a6bf9940d41ce40fd9a0849de1c16f0e362
georgidelchev <alienguymc@gmail.com>
2020-04-25 17:24:41 +0300

    add files via upload

---
+++ [EASY SECTION]/010 - [DcooooooooooderFan]/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DcooooooooooderFan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Dc");
+            for (int i = 0; i < number; i++)
+            {
+                Console.Write("o");
+            }
+            Console.Write("der");
+        }
+    }
+}
