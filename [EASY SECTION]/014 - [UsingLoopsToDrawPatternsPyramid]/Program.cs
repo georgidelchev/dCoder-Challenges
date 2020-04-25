c9019c91f408c9359d506a75e09eb755bd7c3330
georgidelchev <alienguymc@gmail.com>
2020-04-25 18:11:56 +0300

    add files via upload

---
+++ [EASY SECTION]/014 - [UsingLoopsToDrawPatternsPyramid]/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UsingLoopsToDrawPatternPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 5; i > 0; i--)
+            {
+                for (int j = 1; j < i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j < 12 - 2 * i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
