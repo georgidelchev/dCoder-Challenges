2bac6c1f575f4245e27f1bb354d4641fdd45d93e
georgidelchev <alienguymc@gmail.com>
2020-04-26 17:18:28 +0300

    add files via upload

---
+++ [EASY SECTION]/018 - [AvikAndHisGirlfriend'sNecklace]/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace AvikAndHisGirlfriendNecklace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pearlsCount = int.Parse(Console.ReadLine());
+            int[] pearlsNumbers = new int[pearlsCount - 1];
+
+            pearlsNumbers = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Array.Sort(pearlsNumbers);
+
+            foreach (var values in pearlsNumbers)
+            {
+                Console.Write($"{values} ");
+            }
+        }
+    }
+}
