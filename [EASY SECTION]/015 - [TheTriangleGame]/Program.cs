c93418b7a7dfa4f3e5f8a5534646e73fd85e2222
georgidelchev <alienguymc@gmail.com>
2020-04-26 16:26:33 +0300

    add files via upload

---
+++ [EASY SECTION]/015 - [TheTriangleGame]/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TheTriangleGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            for (int i = 0; i < word.Length; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(word[j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
