e36cb5b6261859e1d1bd84030c3d27ef83222f3b
georgidelchev <alienguymc@gmail.com>
2020-04-26 17:34:35 +0300

    add files via upload

---
+++ [EASY SECTION]/020 - [ToggleMe]/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ToggleMe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            string convertedWord = string.Empty;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] >= 'A' && word[i] <= 'Z')
+                {
+                    convertedWord += Char.ToLower(word[i]);
+                }
+                else if (word[i] >= 'a' && word[i] <= 'z')
+                {
+                    convertedWord += Char.ToUpper(word[i]);
+                }
+                else
+                {
+                    convertedWord += word[i];
+                }
+            }
+            
+            Console.WriteLine(convertedWord);
+        }
+    }
+}
