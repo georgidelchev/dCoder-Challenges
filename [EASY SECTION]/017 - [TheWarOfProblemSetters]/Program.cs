e7459682003e591859dff04612b812db7fdc442a
georgidelchev <alienguymc@gmail.com>
2020-04-26 17:10:39 +0300

    add files via upload

---
+++ [EASY SECTION]/017 - [TheWarOfProblemSetters]/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace TheWarOfProblemSetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] garrysPoints = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int garrysSum = garrysPoints.Sum();
+
+            int[] sharrysPoints = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int sharrysSum = sharrysPoints.Sum();
+
+            if (garrysSum > sharrysSum)
+            {
+                double diff = garrysSum - sharrysSum;
+                Console.WriteLine($"Garry {diff}");
+            }
+            else if (sharrysSum > garrysSum)
+            {
+                double diff = sharrysSum - garrysSum;
+                Console.WriteLine($"Sharry {diff}");
+            }
+            else
+            {
+                Console.WriteLine("None 0");
+            }
+        }
+    }
+}
