99c982da3e8011e9fd18095fb713669ea0e4053c
georgidelchev <alienguymc@gmail.com>
2020-04-26 20:10:14 +0300

    add files via upload

---
+++ [EASY SECTION]/008 - [LearningPrimeNumbersWithLoopRecursion]/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace LearningPrimeNumbersWithLoopRecursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int lastValue = numbers[numbers.Length - 1];
+            for (int i = numbers[0]; i <= lastValue; i++)
+            {
+                int counter = 0;
+                for (int j = 2; j <= i / 2; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        counter++;
+                        break;
+                    }
+                }
+                if (counter == 0 && i != 1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+    }
+}
