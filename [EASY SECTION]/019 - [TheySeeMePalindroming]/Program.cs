38f5b9fb7f53d8df318e9e606ae61e40cb2f80c8
georgidelchev <alienguymc@gmail.com>
2020-04-26 17:24:37 +0300

    add files via upload

---
+++ [EASY SECTION]/019 - [TheySeeMePalindroming]/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TheySeeMePalindroming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            string reversedWord = string.Empty;
+
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reversedWord += word[i];
+            }
+
+            if (reversedWord == word)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
