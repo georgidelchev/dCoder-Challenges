65631496ce0794a5bbb0c224432cdeb33c39c0ee
georgidelchev <alienguymc@gmail.com>
2020-04-26 16:51:56 +0300

    add files via upload

---
+++ [EASY SECTION]/016 - [TheSumOfArray]/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace TheSumOfArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int arrayElementsCount = int.Parse(Console.ReadLine());
+            int[] array = new int[arrayElementsCount - 1];
+
+            array = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(array.Sum());
+        }
+    }
+}
