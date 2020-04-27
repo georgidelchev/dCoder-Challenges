ad998e33d15d56cd51d43c0358b359f91c938f3b
georgidelchev <alienguymc@gmail.com>
2020-04-27 20:54:13 +0300

    add files via upload

---
+++ [EASY SECTION]/021 - [TheIrritatingStudents]/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TheIrritatingStudents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testCasesCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < testCasesCount; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine($"{number / 2} {number / 2}");
+                }
+                else
+                {
+                    Console.WriteLine($"{number / 2} {number / 2 + 1}");
+                }
+            }
+        }
+    }
+}
