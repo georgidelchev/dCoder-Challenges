d58691bae59096a58e240b378406164cb29e1566
georgidelchev <alienguymc@gmail.com>
2020-04-28 15:24:59 +0300

    add files via upload

---
+++ [EASY SECTION]/025 - [TheClosestToAverage]/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace TheClosestToAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int arrElements = int.Parse(Console.ReadLine());
+            double[] arr = Console
+                .ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToArray();
+
+            double sum = arr.Sum();
+            int length = arr.Length;
+            double average = sum / length;
+            Console.WriteLine(Math.Round(average));
+        }
+    }
+}
