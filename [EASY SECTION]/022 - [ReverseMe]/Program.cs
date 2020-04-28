1d0bc768824dad03103d6cb9a14a707922221644
georgidelchev <alienguymc@gmail.com>
2020-04-28 12:42:23 +0300

    add files via upload

---
+++ [EASY SECTION]/022 - [ReverseMe]/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ReverseMe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int leftPart = number;
+            int reversedNumber = 0;
+            while (leftPart > 0)
+            {
+                int a = leftPart % 10;
+                reversedNumber = reversedNumber * 10 + a;
+                leftPart = leftPart / 10;
+            }
+            Console.WriteLine(reversedNumber);
+        }
+    }
+}
