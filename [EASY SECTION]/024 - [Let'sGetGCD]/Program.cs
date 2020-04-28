e291d885aadd2c0dd14cdb74f196f6fa6115c61c
georgidelchev <alienguymc@gmail.com>
2020-04-28 14:56:25 +0300

    add files via upload

---
+++ [EASY SECTION]/024 - [Let'sGetGCD]/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LetsGetGCD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            int greatestDivisor = int.MinValue;
+            for (int i = 1; i <= 10; i++)
+            {
+                if (firstNumber % i == 0 && secondNumber % i == 0)
+                {
+                    if (i > greatestDivisor)
+                    {
+                        greatestDivisor = i;
+                    }
+                }
+            }
+            Console.WriteLine(greatestDivisor);
+        }
+    }
+}
