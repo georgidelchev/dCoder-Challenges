8cca1714ab9dc488c26b5f4fa1bcc2dd0ec03750
georgidelchev <alienguymc@gmail.com>
2020-04-28 13:07:23 +0300

    add files via upload

---
+++ [EASY SECTION]/023 - [ThePredictor]/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ThePredictor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string binaryNumber = Console.ReadLine();
+            int zerosCounter = 0;
+            int oneCounter = 0;
+            for (int i = 0; i < binaryNumber.Length; i++)
+            {
+                if (Equals(binaryNumber[i].ToString(), "0"))
+                {
+                    zerosCounter++;
+                }
+                else if (Equals(binaryNumber[i].ToString(), "1"))
+                {
+                    oneCounter++;
+                }
+            }
+            if (zerosCounter == 6 || oneCounter == 6)
+            {
+                Console.WriteLine("Bad");
+            }
+            else
+            {
+                Console.WriteLine("Good");
+            }
+        }
+    }
+}
