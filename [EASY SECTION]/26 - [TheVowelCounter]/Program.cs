15c280ecb6ab60f064820ba1b00ab78c896e0b55
georgidelchev <alienguymc@gmail.com>
2020-05-05 19:53:53 +0300

    add files via upload

---
+++ [EASY SECTION]/26 - [TheVowelCounter]/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TheVowelCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            int totalVowelsCounter = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if ((str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')||
+                    (str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U'))
+                {
+                    totalVowelsCounter++;
+                }
+            }
+            Console.WriteLine(totalVowelsCounter);
+        }
+    }
+}
