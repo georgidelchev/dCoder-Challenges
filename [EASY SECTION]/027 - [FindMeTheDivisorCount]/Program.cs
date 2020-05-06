885d44ed53532c053c67eb2fa9cae3c86c5ce4fe
georgidelchev <alienguymc@gmail.com>
2020-05-06 17:25:33 +0300

    add files via upload

---
+++ [EASY SECTION]/027 - [FindMeTheDivisorCount]/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace FindMeTheDivisorCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int firstNumber = arr[0];
+            int secondNumber = arr[1];
+            int thirdNumber = arr[2];
+
+            int counter = 0;
+            for (int i = firstNumber; i <= secondNumber; i++)
+            {
+                if ((i % thirdNumber) == 0)
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
