ebca78ca17328642a8413da259ab1c4270f94111
georgidelchev <alienguymc@gmail.com>
2020-05-06 19:54:03 +0300

    add files via upload

---
+++ [EASY SECTION]/032 - [TheSecureFunctionXr]/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TheSecureFunctionXr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine(x % 7);
+        }
+    }
+}
