ccf6caab13db47cc7b23d3b9b4454b20f3d33c5b
georgidelchev <alienguymc@gmail.com>
2020-05-06 18:27:28 +0300

    add files via upload

---
+++ [EASY SECTION]/030 - [I'llBeTheNext]/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace IwillBeTheNext
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            for (int i = 0; i < str.Length; i++)
+            {
+                int currentLetter = str[i] + 1;
+                Console.Write((char)currentLetter);
+            }
+        }
+    }
+}
