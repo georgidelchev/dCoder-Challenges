ff4619929364000cd9b1beb8e4b12874be1a2c95
georgidelchev <alienguymc@gmail.com>
2020-05-06 17:58:08 +0300

    add files via upload

---
+++ [EASY SECTION]/029 - [CapitalizeMe]/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace CapitalizeMe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str = Console
+                .ReadLine()
+                .Split()
+                .ToArray();
+            
+            TextInfo txtInfo = new CultureInfo("en-US", false).TextInfo;
+            
+            foreach (var item in str)
+            {
+                Console.Write($"{txtInfo.ToTitleCase(item)} ");
+            }
+        }
+    }
+}
