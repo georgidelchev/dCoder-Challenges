d639046a597e77db05ab0ce260ab4b3421a2a4ec
georgidelchev <alienguymc@gmail.com>
2020-05-06 17:36:28 +0300

    add files via upload

---
+++ [EASY SECTION]/028 - [SquareOfSquares]/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SquareOfSquares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            bool flag = false;
+            Math.Abs(number);
+            for (int i = 2; i < number; i++)
+            {
+                if ((number / i == i) && (number % i == 0))
+                {
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag == true)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
